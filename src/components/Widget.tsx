db67106beefe1658e0c602338b1e737279c5b339
Christoffer Cavalcante <christoffercavalcante@gmail.com>
2022-06-10 23:50:56 -0300

    config tailwind e phosphor icons

---
+++ src/components/Widget.tsx
@@ -0,0 +1,9 @@
+import { ChatTeardropDots } from 'phosphor-react'
+
+export const Widget = () => {
+    return (
+    <button className='bg-brand-500 -500 rounded-full text-white px-3 h-12'>
+        <ChatTeardropDots className='w-6 h-6' />
+    </button>
+    )
+}
